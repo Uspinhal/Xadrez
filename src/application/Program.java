c881b9246602b1e3c2751e715f56b5aac340183a
Rodrigo Barbosa <uspinhal@yahoo.com>
2020-12-05 21:00:24 -0300

    Moving pieces

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,30 @@
 package application;
 
+import java.util.Scanner;
+
+import javax.imageio.plugins.tiff.ExifTIFFTagSet;
+
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Program {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in); 
         ChessMatch chessMatch = new ChessMatch();
+        
+        while (true) {
+            UI.printBoard(chessMatch.getPieces());
+            System.out.println();
+            System.out.println("Source: ");
+            ChessPosition source = UI.readChessPosition(sc);
+            System.out.println();
+            System.out.println("Target: ");
+            ChessPosition target = UI.readChessPosition(sc);
+            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
 
-        UI.printBoard(chessMatch.getPieces());
+        }
+        
 
     }
 }
--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -52,6 +52,19 @@ public class Board {
         piece.position = position;
     }
 
+    public Piece removePiece(Position position) {
+        if (!Boolean.TRUE.equals(positionExists(position))) {
+            throw new BoardException("Position not on the board!");
+        }
+        if (this.piece(position) == null) {
+            return null;
+        }
+        Piece aux = piece(position);
+        aux.position = null;
+        pieces[position.getRow()][position.getColumn()] = null;
+        return aux;
+    }
+
     public Boolean positionExists(int row, int column) {
         return row >= 0 && row < this.rows && column >= 0 && column < this.columns;
     }
